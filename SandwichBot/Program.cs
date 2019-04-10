9538e30d74432812a3a90d5f499bc5d343666edf
HoLLy <dario.db@hotmail.com>
2019-04-10 18:58:22 +0200

    Add runnable project to ease deployment

---
+++ SandwichBot/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SandwichBot
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args) => await HoLLy.DiscordBot.Program.Main();
+    }
+}
