dce530f5bb6a2c84047c64e80512dc2867abbb38
HoLLy <dario.db@hotmail.com>
2019-04-19 01:58:23 +0200

    Add roll command

--- SandwichBot.Commands/Commands.cs
+++ SandwichBot.Commands/Commands.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord.WebSocket;
 using HoLLy.DiscordBot.Commands;
@@ -80,6 +80,31 @@ namespace HoLLy.DiscordBot.Sandwich
                 return Task.CompletedTask;
             };
             cl.StopAsync().Wait();
+        }
+
+        [Command("roll")]
+        public static string Roll(string arg)
+        {
+            var split = arg.Split(' ');
+            var rand = new Random();
+
+            if (Regex.IsMatch(split[0], @"^\d{1,9}d\d{1,9}$")) {
+                var split2 = split[0].Split('d');
+                if (int.TryParse(split2[0], out int count) && int.TryParse(split2[1], out int max)) {
+                    if (count > 20) return "That's a bit much, isn't it?";
+
+                    var sb = new StringBuilder();
+                    for (int i = 0; i < count; i++) {
+                        sb.Append(rand.Next(max));
+                        sb.Append(" ");
+                    }
+
+                    return sb.ToString();
+                }
+            }
+
+            // if no ndn was given, just use parameter as 1dn
+            return rand.Next(int.TryParse(split[0], out int maxValue) ? maxValue : 100).ToString();
         }
     }
 }
